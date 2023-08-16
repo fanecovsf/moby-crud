b9edde5e8f1bfc68e12131a3e1c2ad8cb00c44da
fanecovsf <gustavo_h07@hotmail.com>
2023-08-16 09:56:11 -0300

    ✅ Fix: Adicionado STATIC_ROOT

--- moby/moby/settings.py
+++ moby/moby/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,6 +153,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'minalba/static',
     BASE_DIR / 'vibra/static',
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
